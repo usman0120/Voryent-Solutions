e2d9eb2e8ddd78c0a0d92a03a4c9c1e4ee9e1011
Usman Ahmad <207910452+usman0120@users.noreply.github.com>
2026-09-20 16:29:18 +0500

    fix tailwind error

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -1,46 +1,43 @@
 import type { Config } from "tailwindcss";
-import { tailwindPreset } from "@voryent/config";
+import { tailwindPreset } from "../../packages/config/src";
 
 const config = {
-    darkMode: ["class"],
-    presets: [tailwindPreset],
-  content: [
-    "./src/**/*.{ts,tsx}",
-    "../../packages/ui/src/**/*.{ts,tsx}",
-  ],
-    theme: {
-    	extend: {
-    		keyframes: {
-    			'accordion-down': {
-    				from: { height: '0' },
-    				to: { height: 'var(--radix-accordion-content-height)' }
-    			},
-    			'accordion-up': {
-    				from: { height: 'var(--radix-accordion-content-height)' },
-    				to: { height: '0' }
-    			},
-          'fade-in': {
-            from: { opacity: '0', transform: 'translateY(10px)' },
-            to: { opacity: '1', transform: 'translateY(0)' },
-          },
-          'fade-in-up': {
-            from: { opacity: '0', transform: 'translateY(20px)' },
-            to: { opacity: '1', transform: 'translateY(0)' },
-          },
-          'marquee': {
-            from: { transform: 'translateX(0)' },
-            to: { transform: 'translateX(calc(-100% - 3rem))' },
-          }
-    		},
-    		animation: {
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out',
-          'fade-in': 'fade-in 0.5s ease-out',
-          'fade-in-up': 'fade-in-up 0.7s ease-out forwards',
-          'marquee': 'marquee 30s linear infinite',
-    		}
-    	}
-    }
+  darkMode: ["class"],
+  presets: [tailwindPreset],
+  content: ["./src/**/*.{ts,tsx}", "../../packages/ui/src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "fade-in": {
+          from: { opacity: "0", transform: "translateY(10px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        "fade-in-up": {
+          from: { opacity: "0", transform: "translateY(20px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - 3rem))" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.7s ease-out forwards",
+        marquee: "marquee 30s linear infinite",
+      },
+    },
+  },
 } satisfies Config;
 
 export default config;
